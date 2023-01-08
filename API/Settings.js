067e38a872e27eb9024104dba21fab396fc22718
eliot martin <eliot.martin@epitech.eu>
2023-01-08 16:33:06 +0100

    :bug: API patch username

--- API/Settings.js
+++ API/Settings.js
@@ -24,7 +24,7 @@ export async function changePassword (email, password, code) {
 
 export async function patchParams (id, values) {
     console.log(id, values)
-    return await fetch(`http://52.166.128.133/user?id=` + id, {
+    return await fetch(`http://52.166.128.133/user/?id=` + id, {
         method: 'PATCH',
         body: JSON.stringify(values),
         headers: {
