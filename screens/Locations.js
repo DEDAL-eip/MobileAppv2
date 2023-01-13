c8047ca21476834072cb336b64ce94a26dd9fedb
serapagranchose <seraphin.perrot@gmail.com>
2023-01-13 16:56:59 +0100

    :dizzy: Adding animation when a location is selected

--- screens/Locations.js
+++ screens/Locations.js
@@ -1,20 +1,55 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ScrollView, Text, View } from "react-native";
+import { useState } from "react";
+
 import { Separator } from "../components/Separator";
 import LocationCard from '../components/LocationCard';
 import { GlobalButton } from "../components/Button";
 
+import { getLocations } from "../API/Locations";
+
 import { global } from "../style/styles";
 
 export default function Location() {
-  const APIlocations = [
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
-  ]
+  const [Locations, setLocations] = useState([])
+  const data = []
+
+  function fetchLocationsInItinerary() {
+    console.log("TROC", SafeAreaProvider.itinerary)
+    setLocations(SafeAreaProvider.itinerary)
+  }
+
+  function fetchLocationsOutItinerary() {
+    setLocations([
+      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+      ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
+    ])
+  }
+
+  function fetchLocationsOutFilters() {
+    setLocations([
+      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+      ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
+    ])
+  }
+
+  const assertToItinerary = (location, push) => {
+    if (push == true)
+      data.push(location);
+    else
+      data.pop(location);
+    SafeAreaProvider.itinerary = data
+    console.log("SAFEARE ITINERARY AFTER ASSERT", SafeAreaProvider.itinerary, ", push:", push, ", location:", location)
+  }
 
   return (
     <View style={global.container}>
@@ -23,12 +58,12 @@ export default function Location() {
         <Separator />
       </View>
       <View style={global.middleContainer}>
-        <GlobalButton title='In itinerary' onPress={() => console.log('In itinerary')} />
-        <GlobalButton title='Out itinerary' onPress={() => console.log('Out itinerary')} />
-        <GlobalButton title='Out filters' onPress={() => console.log('Out filters')} />
+        <GlobalButton title='In itinerary' onPress={() => fetchLocationsInItinerary()} />
+        <GlobalButton title='Out itinerary' onPress={() => fetchLocationsOutItinerary()} />
+        <GlobalButton title='Out filters' onPress={() => fetchLocationsOutFilters()} />
         <ScrollView>
-          {APIlocations.map((item, index) => {
-            return <LocationCard key={index} name={item[0]} description={item[1]} />;
+          {Locations.map((item, index) => {
+            return <LocationCard key={index} assertToItinerary={assertToItinerary} name={item[0]} description={item[1]} />;
           })}
         </ScrollView>
       </View>
