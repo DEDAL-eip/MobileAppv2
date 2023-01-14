06537828925fb9215c87bd27be42aec04b473d39
serapagranchose <seraphin.perrot@gmail.com>
2023-01-14 18:11:59 +0100

    :art: Remove useless line and the code is more simple in exchange of animations

--- screens/Locations.js
+++ screens/Locations.js
@@ -1,6 +1,6 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ScrollView, Text, View } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { Separator } from "../components/Separator";
 import LocationCard from '../components/LocationCard';
@@ -11,44 +11,46 @@ import { getLocations } from "../API/Locations";
 import { global } from "../style/styles";
 
 export default function Location() {
+  const [Selection, setSelection] = useState("")
   const [Locations, setLocations] = useState([])
-  const data = []
 
-  function fetchLocationsInItinerary() {
-    console.log("TROC", SafeAreaProvider.itinerary)
-    setLocations(SafeAreaProvider.itinerary)
-  }
+  const locationsOutItinerary  = [
+    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+    ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
+  ]
+  const locationsOutFilters = [
+    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
+    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
+    ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
+  ]
+  const locationsInItinerary = []
 
-  function fetchLocationsOutItinerary() {
-    setLocations([
-      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-      ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
-    ])
-  }
-
-  function fetchLocationsOutFilters() {
-    setLocations([
-      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-      ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-      ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-      ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
-    ])
-  }
-
-  const assertToItinerary = (location, push) => {
-    if (push == true)
-      data.push(location);
-    else
-      data.pop(location);
-    SafeAreaProvider.itinerary = data
-    console.log("SAFEARE ITINERARY AFTER ASSERT", SafeAreaProvider.itinerary, ", push:", push, ", location:", location)
+  const assertToItinerary = (location) => {
+    console.log("Selection: ", Selection)
+    switch(Selection) {
+      case 'Out filters':
+        location[0] = "Out filters: " + location[0]
+        locationsOutFilters.splice(locationsOutFilters.indexOf(location))
+        locationsInItinerary.push(location)
+        break
+      case 'Out itinerary':
+        location[0] = "Out itinerary: " + location[0]
+        locationsOutItinerary.splice(locationsOutItinerary.indexOf(location))
+        locationsInItinerary.push(location)
+        break
+      case 'In itinerary':
+        locationsInItinerary.splice(locationsInItinerary.indexOf(location))
+        break
+    }
+    SafeAreaProvider.itinerary = locationsInItinerary
   }
 
   return (
@@ -58,9 +60,18 @@ export default function Location() {
         <Separator />
       </View>
       <View style={global.middleContainer}>
-        <GlobalButton title='In itinerary' onPress={() => fetchLocationsInItinerary()} />
-        <GlobalButton title='Out itinerary' onPress={() => fetchLocationsOutItinerary()} />
-        <GlobalButton title='Out filters' onPress={() => fetchLocationsOutFilters()} />
+        <GlobalButton title='In itinerary' disable={Selection === 'In itinerary'} onPress={() => {
+          setSelection('In itinerary')
+          setLocations(SafeAreaProvider.itinerary)
+        }} />
+        <GlobalButton title='Out itinerary' disable={Selection === 'Out itinerary'} onPress={() => {
+          setSelection('Out itinerary')
+          setLocations(locationsOutItinerary)
+        }} />
+        <GlobalButton title='Out filters' disable={Selection === 'Out filters'} onPress={() => {
+          setSelection('Out filters')
+          setLocations(locationsOutFilters)
+        }} />
         <ScrollView>
           {Locations.map((item, index) => {
             return <LocationCard key={index} assertToItinerary={assertToItinerary} name={item[0]} description={item[1]} />;
