b529b717b3216d71b38a99bf703785914aeb3bff
serapagranchose <seraphin.perrot@gmail.com>
2023-01-24 09:28:32 +0100

    :sparkles: adding submenu to verify an account on signup

--- screens/Login.js
+++ screens/Login.js
@@ -2,7 +2,7 @@ import { GlobalButton } from "../components/Button";
 import { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { Text, View } from "react-native";
-import easyLog, { signIn, signUp, google } from "../API/Login";
+import easyLog, { signIn, signUp, signUpCode, google } from "../API/Login";
 import { color, global, textInput } from "../style/styles";
 import { HomeTitle } from "../components/Title";
 import Colors from "../constants/Colors"
@@ -13,8 +13,10 @@ import * as Linking from 'expo-linking'
 
 export default function Login() {
   const [Error, setError] = useState(false)
+  const [Step, setStep] = useState("")
   const [Email, setEmail] = useState("")
   const [Password, setPassword] = useState("")
+  const [VerificationCode, setVerificationCode] = useState("")
 
 
   async function CallAPI() {
@@ -35,6 +37,25 @@ export default function Login() {
     }
   }
 
+  async function createAccount(email, password) {
+    const res = await signUp(email, password)
+    if (res.hasError == true)
+      setError(true)
+    else {
+      setStep("VerificationCode")
+    }
+  }
+
+  async function verifyAccount(email, code) {
+    const res = await signUpCode(email, code)
+    if (res.hasError == true)
+      setError(true)
+    else {
+      SafeAreaProvider.Loged(true)
+      SafeAreaProvider.Log = res
+    }
+  }
+
   const createGoogleAccount = async () => {
     let callbackUrl  = Linking.createURL('/')
     await WebBrowser.openBrowserAsync(`https://dedal.auth.eu-west-1.amazoncognito.com/login?client_id=cse59djt26m2kikuacurl26uj&response_type=code&scope=email+openid+profile&redirect_uri=${callbackUrl}`)
@@ -45,9 +66,10 @@ export default function Login() {
     let res = await google(code, callbackUrl)
     if(res.error)
       setError(true)
-    else 
-    SafeAreaProvider.Loged(true)
-    SafeAreaProvider.Log = res
+    else {
+      SafeAreaProvider.Loged(true)
+      SafeAreaProvider.Log = res
+    }
 }
 
 const url = Linking.useURL();
@@ -67,17 +89,30 @@ if (url) {
         <HomeTitle title='DEDAL' pict={require('../assets/logo.png')} subtitle='Le chemin de votre culture'/>
       </View>
       <View style={global.middleContainer}>
-        <View style={[global.basicContainer, {paddingBottom:20}]}>
-          <Text style={color.errorRed}>{Error ? "Email ou mot de passe incorrecte" : ""}</Text>
-          <TextInputGlobal autoCapitalize='none' autoComplete='email' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Email" onChangeText={setEmail} value={Email}></TextInputGlobal>
-          <TextInputPassword autoCapitalize='none' autoComplete='password' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Passord" onChangeText={setPassword} value={Password}></TextInputPassword>
-        </View>
-          <GlobalButton title="Sign In" onPress={() => EasySignIn(Email, Password)}></GlobalButton>
-          <View style={{width: '25%',paddingBottom:20, paddingTop:60}}>
-            <Separator />
-          </View>
-            <GlobalButton title="Sign Up" onPress={() => EasySignIn('eliot.martin@hotmail.fr', 'pasWORD1@')}></GlobalButton>
+        {Step === "VerificationCode" ?
+          <>
+            <View style={[global.basicContainer, {paddingBottom:20}]}>
+              <Text style={Error ? color.errorRed : null}>{Error ? "Wrong verification code." : "A verification code was sent to your email adress."}</Text>
+              <TextInputGlobal autoCapitalize='none' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Verification Code" onChangeText={setVerificationCode} value={VerificationCode}></TextInputGlobal>
+            </View>
+            <GlobalButton title="Verify" onPress={() => verifyAccount(Email, VerificationCode)}></GlobalButton>
+          </>
+        :
+          <>
+            <View style={[global.basicContainer, {paddingBottom:20}]}>
+              <Text style={Error ? color.errorRed : null}>{Error ? "Email ou mot de passe incorrecte" : ""}</Text>
+              <TextInputGlobal autoCapitalize='none' autoComplete='email' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Email" onChangeText={setEmail} value={Email}></TextInputGlobal>
+              <TextInputPassword autoCapitalize='none' autoComplete='password' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Password" onChangeText={setPassword} value={Password}></TextInputPassword>
+            </View>
+            
+            <GlobalButton title="Sign In" onPress={() => EasySignIn(Email, Password)}></GlobalButton>
+            <GlobalButton title="Sign Up" onPress={() => createAccount(Email, Password)} />
+            <View style={{width: '25%',paddingBottom:20, paddingTop:20}}>
+              <Separator />
+            </View>
             <GlobalButton title="Google" onPress={() => createGoogleAccount()}></GlobalButton>
+          </>
+        }
       </View>
     </View>
   );
