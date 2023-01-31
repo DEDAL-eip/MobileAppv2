d15424012c80a71215162c07cd0de37d815b8098
eliot martin <eliot.martin@epitech.eu>
2023-01-31 18:48:19 +0100

    :fire: simplify login.js

--- screens/Login.js
+++ screens/Login.js
@@ -2,7 +2,7 @@ import { GlobalButton } from "../components/Button";
 import { useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { View, Text } from "../constants/Themed";
-import easyLog, { signIn, signUp, signUpCode, google, nextG } from "../API/Login";
+import { signIn, google, nextG } from "../API/Login";
 import { color, global, textInput } from "../style/styles";
 import { HomeTitle } from "../components/Title";
 import Colors from "../constants/Colors"
@@ -25,18 +25,8 @@ export default function Login() {
   const [Step, setStep] = useState(0)
   const [Email, setEmail] = useState("")
   const [Password, setPassword] = useState("")
-  const [VerificationCode, setVerificationCode] = useState("")
-  
-  const url = Linking.useURL()
 
-  async function CallAPI() {
-    const res = await easyLog()
-    if (res == 200) {
-      return true
-    }
-    else 
-      return false
-  }
+  const url = Linking.useURL()
 
   async function EasySignIn(email, password) {
     const res = await signIn(email, password)
@@ -48,83 +38,60 @@ export default function Login() {
     }
   }
 
-  async function createAccount(email, password) {
-    const res = await signUp(email, password)
-    if (res.hasError == true)
-      setError(true)
-    else {
-      setStep("VerificationCode")
-    }
-  }
-  
-  async function verifyAccount(email, code) {
-    const res = await signUpCode(email, code)
-    if (res.hasError == true)
-    setError(true)
-    else {
-      EasySignIn('eliot.martin@hotmail.fr', 'pasWORD1@')
-      SafeAreaProvider.Loged(true)
-      SafeAreaProvider.Log = res
-    }
-  }
-
   const createGoogleAccount = async () => {
-    let callbackUrl  = Linking.createURL()
-    console.log(callbackUrl)
+    let callbackUrl = Linking.createURL()
     await WebBrowser.openBrowserAsync(`https://dedal.auth.eu-west-1.amazoncognito.com/login?client_id=cse59djt26m2kikuacurl26uj&response_type=code&scope=email+openid+profile&redirect_uri=${callbackUrl}`)
-}
+  }
 
   const logInGoogleAccount = async (code) => {
-    let callbackUrl  = Linking.createURL()
+    let callbackUrl = Linking.createURL()
     let res = await google(code, callbackUrl)
-    if(res.error)
+    if (res.error)
       setError(true)
     else
       nextGoogle(res)
-}
-
-const nextGoogle = async (res) => {
-  let result = await nextG(res)
-  if (result.error)
-    setError(true)
-  else {
-    SafeAreaProvider.Loged(true)
-    SafeAreaProvider.Log = result
   }
-}
 
-useEffect(() => {
-  if (url) {
-    const {queryParams} = Linking.parse(url);
-    if (queryParams.code)
-      logInGoogleAccount(queryParams.code)
+  const nextGoogle = async (res) => {
+    let result = await nextG(res)
+    if (result.error)
+      setError(true)
+    else {
+      SafeAreaProvider.Loged(true)
+      SafeAreaProvider.Log = result
+    }
   }
-}, [url])
+
+  useEffect(() => {
+    if (url) {
+      const { queryParams } = Linking.parse(url);
+      if (queryParams.code)
+        logInGoogleAccount(queryParams.code)
+    }
+  }, [url])
 
   return (
     <View style={global.container}>
-      <View style={{width : 10, height: 10, alignSelf: 'flex-end', borderRadius: 100, marginRight: 10, marginTop: 10, backgroundColor : CallAPI() ? Colors('ValidateGreen') : Colors('ErrorRed'), }}>
-      </View>
       <View style={global.titleContainer}>
-        <HomeTitle title='DEDAL' pict={require('../assets/logo.png')} subtitle='Le chemin de votre culture'/>
+        <HomeTitle title='DEDAL' pict={require('../assets/logo.png')} subtitle='Le chemin de votre culture' />
       </View>
       <View style={global.middleContainer}>
         {Step === 0 ?
           <>
-            <View style={[global.basicContainer, {paddingBottom:20}]}>
+            <View style={[global.basicContainer, { paddingBottom: 20 }]}>
               <Text style={Error ? color.errorRed : null}>{Error ? "Email ou mot de passe incorrecte" : ""}</Text>
-              <TextInputGlobal autoCapitalize='none' autoComplete='email' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Email" onChangeText={setEmail} value={Email}></TextInputGlobal>
-              <TextInputPassword autoCapitalize='none' autoComplete='password' style={[textInput.global, {borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue')}]} placeholder="Password" onChangeText={setPassword} value={Password}></TextInputPassword>
+              <TextInputGlobal autoCapitalize='none' autoComplete='email' style={[textInput.global, { borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue') }]} placeholder="Email" onChangeText={setEmail} value={Email}></TextInputGlobal>
+              <TextInputPassword autoCapitalize='none' autoComplete='password' style={[textInput.global, { borderColor: Colors(Error ? 'ErrorRed' : 'dedalBlue') }]} placeholder="Password" onChangeText={setPassword} value={Password}></TextInputPassword>
             </View>
-            
+
             <GlobalButton title="Sign In" onPress={() => EasySignIn(Email, Password)}></GlobalButton>
             <GlobalButton title="Sign Up" onPress={() => setStep(1)} />
-            <View style={{width: '25%',paddingBottom:20, paddingTop:20}}>
+            <View style={{ width: '25%', paddingBottom: 20, paddingTop: 20 }}>
               <Separator />
             </View>
             <GlobalButton title="Google" onPress={() => createGoogleAccount()}></GlobalButton>
           </>
-          : <SignUp oldEmail={Email} oldPassword={Password} setBack={() => setStep(0)}/>
+          : <SignUp oldEmail={Email} oldPassword={Password} setBack={() => setStep(0)} />
         }
       </View>
     </View>
