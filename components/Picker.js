32d145ec57eff67cc89f4e9fc7de0d484abb9cae
serapagranchose <seraphin.perrot@gmail.com>
2023-04-03 18:21:12 +0900

    :lipstick: Rework components to fit Figma's mockup

--- components/Picker.js
+++ components/Picker.js
@@ -1,34 +1,44 @@
-import { useColorScheme, View } from "react-native";
+import { useColorScheme, View, Text } from "react-native";
 import { Picker as DefaultPicker } from '@react-native-picker/picker';
 
 import Colors from "../constants/Colors";
 
-
-export function MyPicker(props) {
+export const Picker = (({
+    title,
+    items,
+    selectedValue,
+    onValueChange,
+    disable,
+    style
+}) => {
     const theme = useColorScheme();
 
     return (
-        <View style={{
-            borderRadius : 25,
-            borderWidth: 2,
-            borderColor: Colors('dedalBlue'),
-            overflow: 'hidden'
-        }}>
-            <DefaultPicker
-                mode="dropdown"
-                dropdownIconColor={Colors('dedalBlue')}
-                selectedValue={props.selectedValue}
-                onValueChange={(itemValue, itemIndex) => {
-                    props.onValueChange(itemValue)
-                }}
-                style={{ height: 50, color: Colors('Text', theme) }}
-            >
-                {props.items.map((item, i) => {
-                    return (
-                        <DefaultPicker.Item key={i} label={item[0]} value={item[1]} />
-                    )
-                })}
-            </DefaultPicker>
+        <View style={{width : '90%', margin: 10}}>
+            <Text style={[{ color: Colors('Text', theme), fontWeight: 'bold'}]}>{title}</Text>
+            <View style={{
+                borderRadius : 25,
+                borderWidth: 2,
+                borderColor: Colors('dedalBlue'),
+                overflow: 'hidden'
+            }}>
+                <DefaultPicker
+                    mode="dropdown"
+                    dropdownIconColor={Colors('dedalBlue')}
+                    selectedValue={selectedValue}
+                    onValueChange={(itemValue, itemIndex) => {
+                        onValueChange(itemValue)
+                    }}
+                    style={{ height: 50, color: Colors('Text', theme) }}
+                    disable={disable}
+                >
+                    {items.map((item, i) => {
+                        return (
+                            <DefaultPicker.Item key={i} label={item[0]} value={item[1]} />
+                        )
+                    })}
+                </DefaultPicker>
+            </View>
         </View>
     )
-}
+})
