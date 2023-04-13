2fb9ecb1d791cdd1953afc346ec0808c73a9636a
eliot martin <eliot.martin@epitech.eu>
2023-04-13 14:52:11 +0200

    :sparkles: update location page

--- screens/Locations.js
+++ screens/Locations.js
@@ -1,14 +1,11 @@
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { ScrollView, View } from "react-native";
-import { useState } from "react";
+import { ScrollView, View, Text } from "react-native";
+import { useEffect, useState } from "react";
 
 import LocationCard from '../components/LocationCard';
 import { TextButton } from "../components/buttons/TextButton";
-
-import { getLocations } from "../API/Locations";
-
-import { global } from "../style/styles";
-
+import { getLocationOut, getLocationIn } from "../API/Locations"
+import { global, shadow, button } from "../style/styles";
 import '../constants/languages/i18n';
 import { useTranslation } from 'react-i18next';
 
@@ -22,33 +19,17 @@ import { useTranslation } from 'react-i18next';
 export default function Location() {
   const [Selection, setSelection] = useState("")
   const [Locations, setLocations] = useState([])
-  const {t, i18n} = useTranslation();
-
-  const locationsOutItinerary  = [
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
-  ]
-  const locationsOutFilters = [
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Le palais des beaux-arts', "Un musée municipal d'art et d'antiquités situé place de la République à Lille, dans la région Hauts-de-France."],
-    ['Epitech Lille', "L'école de l'expertise informatique et de l'innovation."],
-    ['Place du Général-de-Gaulle', "Un espace public urbain de la commune de Lille dans le département français du Nord en région Hauts-de-France."]
-  ]
-  const locationsInItinerary = []
+  const { t, i18n } = useTranslation();
+  const [display, setDisplay] = useState(1)
+  console.log(SafeAreaProvider.Place.map(elem => elem.name))
 
   /**
    * Hook to push or pop filter from Selection to or from itinerary
    * set locationsInItinerary in SafeAreaProvider.itinerary
    */
   const assertToItinerary = (location) => {
-    switch(Selection) {
+    return
+    switch (Selection) {
       case 'Out filters':
         location[0] = "Out filters: " + location[0]
         locationsOutFilters.splice(locationsOutFilters.indexOf(location))
@@ -66,28 +47,75 @@ export default function Location() {
     SafeAreaProvider.itinerary = locationsInItinerary
   }
 
+  const ManageDisplay = () => {
+    const result = 1
+    return (result)
+  }
+
+  useEffect(() => {
+    const tmp = async () => {
+      let res1 = await getLocationOut(SafeAreaProvider.Log.id)
+      let res2 = await getLocationIn(SafeAreaProvider.Log.id)
+      console.log('here', res1, res2)
+
+    }
+    tmp()
+  },[])
+
   return (
     <View style={global.container}>
-      <View style={global.middleContainer}>
-        <TextButton title={t('in itinerary')} disable={Selection === 'In itinerary'} onPress={() => {
-          setSelection('In itinerary')
-          setLocations(SafeAreaProvider.itinerary)
-          console.log('Debug => ', SafeAreaProvider.itinerary)
-        }} />
-        <TextButton title={t('out itinerary')} disable={Selection === 'Out itinerary'} onPress={() => {
-          setSelection('Out itinerary')
-          setLocations(locationsOutItinerary)
-        }} />
-        <TextButton title={t('out filters')} disable={Selection === 'Out filters'} onPress={() => {
-          setSelection('Out filters')
-          setLocations(locationsOutFilters)
-        }} />
-        <ScrollView>
-          {Locations.map((item, index) => {
-            return <LocationCard key={index} assertToItinerary={assertToItinerary} name={item[0]} description={item[1]} />;
-          })}
-        </ScrollView>
+      <View style={global.titleContainer}>
+        <View style={[global.header, shadow.Bottom]}>
+          <View style={{ width: "33%", display: 'flex', alignItems: 'center' }}>
+            <TextButton style={button.disable} title={t("Votre itineraire")} onPress={() => setDisplay(2)} />
+          </View>
+          <View style={{ width: "33%", display: 'flex', alignItems: 'center' }}>
+            <TextButton title={t('Interet ?')} onPress={() => setDisplay(1)} />
+          </View>
+          <View style={{ width: "33%", display: 'flex', alignItems: 'center' }}>
+            <TextButton title={t('Proche de vous')} onPress={() => setDisplay(3)} />
+          </View>
+        </View>
+      </View>
+
+      <ScrollView>
+        {SafeAreaProvider.Place.map((item, index) => {
+          return <LocationCard key={index} assertToItinerary={assertToItinerary} item={item} />;
+        })}
+      </ScrollView>
+
+      <View style={global.titleContainer}>
+        <View style={[global.header, shadow.Bottom]}>
+          <View style={{ width: "33%", display: 'flex', alignItems: 'center' }}>
+            <TextButton style={button.disable} title={t("Ajouter")} onPress={() => console.log('add')} />
+          </View>
+        </View>
+        {/* <View style={{ height: '100%', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', }}>
+            {ManageDisplay()}
+          </View> */}
       </View>
     </View>
+    // <View style={global.container}>
+    //   <View style={global.middleContainer}>
+    //     <TextButton title={t('in itinerary')} disable={Selection === 'In itinerary'} onPress={() => {
+    //       setSelection('In itinerary')
+    //       setLocations(SafeAreaProvider.itinerary)
+    //       console.log('Debug => ', SafeAreaProvider.itinerary)
+    //     }} />
+    //     <TextButton title={t('out itinerary')} disable={Selection === 'Out itinerary'} onPress={() => {
+    //       setSelection('Out itinerary')
+    //       setLocations(locationsOutItinerary)
+    //     }} />
+    //     <TextButton title={t('out filters')} disable={Selection === 'Out filters'} onPress={() => {
+    //       setSelection('Out filters')
+    //       setLocations(locationsOutFilters)
+    //     }} />
+    //     <ScrollView>
+    //       {Locations.map((item, index) => {
+    //         return <LocationCard key={index} assertToItinerary={assertToItinerary} name={item[0]} description={item[1]} />;
+    //       })}
+    //     </ScrollView>
+    //   </View>
+    // </View>
   );
 }
